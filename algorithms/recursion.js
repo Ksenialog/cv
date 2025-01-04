b84057b73f5fa9154a9be93fde911d3c568afd1f
Ksenia <kseniarlog@gmail.com>
2025-01-04 04:07:24 +0300

    chore

--- algorithms/recursion.js
+++ algorithms/recursion.js
@@ -44,7 +44,6 @@ console.log(anotherFlat([1, 2, [3, 4, [5, 6, [7, 8, [9, 10]]]]]))
 // fibonacci
 // 0 1 1 2 3 5 8 13 21 34
 const fibonacci = (index) => {
-  debugger
   if (index === 1) return 0
   if (index === 2) return 1
   

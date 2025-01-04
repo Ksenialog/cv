c15b4f33532fa09354decb551fea2d90bb54d7a8
Ksenia <kseniarlog@gmail.com>
2025-01-04 03:44:21 +0300

    Числа Фибоначчи через рекурсию

--- algorithms/recursion.js
+++ algorithms/recursion.js
@@ -40,3 +40,21 @@ console.log(anotherFlat([1, 2, [3, 4]]))
 console.log(anotherFlat([]))
 console.log(anotherFlat([[1, 5], 5, 10]))
 console.log(anotherFlat([1, 2, [3, 4, [5, 6, [7, 8, [9, 10]]]]]))
+
+// fibonacci
+// 0 1 1 2 3 5 8 13 21 34
+const fibonacci = (index) => {
+  debugger
+  if (index === 1) return 0
+  if (index === 2) return 1
+  
+  return fibonacci(index - 1) + fibonacci(index - 2)
+}
+
+console.log(fibonacci(1))
+console.log(fibonacci(2))
+console.log(fibonacci(3))
+console.log(fibonacci(4)) // 2
+console.log(fibonacci(5)) // 3
+console.log(fibonacci(8)) // 13
+console.log(fibonacci(13)) // 144

e527b27ad4d9a79ae8db4e0be70cf9f5d380fcb2
Ksenia <kseniarlog@gmail.com>
2025-01-11 02:49:16 +0300

    быстрая сортировка

---
+++ algorithms/quickSort.js
@@ -0,0 +1,42 @@
+const random = (min, max) => {
+  const interval = max - min
+  const shift = min
+  
+  return Math.round(Math.random() * interval + shift)
+}
+
+const partition = (array, left, right) => {
+  const pivot = array[random(left, right)]
+  
+  while (left < right) {
+    while (array[left] < pivot) {
+      left += 1
+    }
+    
+    while (array[right] > pivot) {
+      right -= 1
+    }
+    
+    if (left <= right) {
+      [array[left], array[right]] = [array[right], array[left]]
+      left += 1
+    }
+  }
+  
+  return left
+}
+
+const quickSort = (array, left = 0, right = array.length - 1) => {
+  const pivotIndex = partition(array, left, right)
+  if (left < pivotIndex - 1) {
+    quickSort(array, left, pivotIndex - 1)
+  }
+  
+  if (pivotIndex < right) {
+    quickSort(array, pivotIndex, right)
+  }
+  
+  return array
+}
+
+console.log(quickSort([56, 87, 18, 92, 42, 31, 44, 82, 36, 91]))

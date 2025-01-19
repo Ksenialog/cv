824097a30b5178014007cf756e3bcecb2d1c2d73
Ksenia <kseniarlog@gmail.com>
2025-01-20 02:42:17 +0300

    пример задачи с 2 указателями

---
+++ algorithms/twoPoints.js
@@ -0,0 +1,24 @@
+const moveZeroes = function(nums) {
+  let first = 0
+  let next = 1
+  
+  while (nums.length > next) {
+    if (nums[first] === 0) {
+      while (next < nums.length) {
+        if (nums[next]) {
+          [nums[first], nums[next]] = [nums[next], nums[first]]
+          first += 1
+        }
+        
+        next += 1
+      }
+    }
+    
+    first += 1
+    next += 1
+  }
+  
+  return nums
+};
+
+console.log(moveZeroes([0,1,0,3,12]))

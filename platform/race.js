a0abaa1a8ade7ef711fe7da192feb9c1e13c7578
Ksenia <kseniarlog@gmail.com>
2025-04-17 22:22:48 +0300

    AbortController

--- platform/race.js
+++ platform/race.js
@@ -8,20 +8,43 @@ const users = [
   { userId: 7, delay: 400 },
 ]
 
-const delay = (data, ms) => new Promise((resolve) => setTimeout(() => resolve(data), ms))
+const delay = (data, ms, signal) => {
+  return new Promise((resolve, reject) => {
+    if (signal.aborted) {
+      reject(new Error('Aborted'))
+      return
+    }
+    
+    const timerId = setTimeout(() => resolve(data), ms)
+    
+    signal.addEventListener('abort', () => {
+      clearTimeout(timerId)
+      reject(new Error('Aborted'))
+    })
+  })
+}
 
 const fetchUsers = async (data, limit, result = []) => {
+  const abortController = new AbortController()
+  const signal = abortController.signal
+  
   if (limit > 0) {
-    await Promise.race([...data.map(({ userId,  delay: myDelay }) => delay(userId,  myDelay))])
+    await Promise.race([...data.map(({ userId,  delay: myDelay }) => delay(userId,  myDelay, signal))])
       .then((data) => {
         result.push(data)
+        abortController.abort()
       })
-
+      .catch((err) => {
+        console.error(err)
+      })
+    
     return fetchUsers(data.filter(({ userId }) => !result.includes(userId)), limit - 1, result)
   }
   
   return result
 }
 
-const test = await fetchUsers(users, 3)
-console.log(test)
+fetchUsers(users, 3)
+  .then((result) => {
+    console.log(result)
+  })
